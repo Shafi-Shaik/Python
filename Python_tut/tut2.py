810cba5f54074d6013ecf9db70e1789fbed2087a
Shafi-1997 <shaikshafi1997@gmail.com>
2021-09-11 15:36:58 +0530

    2nd: added tut2.py variable,datatypes

---
+++ Python_tut/tut2.py
@@ -0,0 +1,16 @@
+#Variables 
+name ='shafi'
+age = 99
+isMarried = False
+gradePoints = 9.12
+
+print(name)
+print(age)
+print(isMarried)
+print(gradePoints)
+
+#type() to identity the datatype of variable
+print(type(name))
+print(type(age))
+print(type(isMarried))
+print(type(gradePoints))
