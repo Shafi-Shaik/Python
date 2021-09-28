778e6dd35fe1f0f6f9cf7e2752901bff56e3a398
Shafi-1997 <shaikshafi1997@gmail.com>
2021-09-28 16:10:02 +0530

    added string functions in SQL folder

--- Python_tut/tut2.py
+++ Python_tut/tut2.py
@@ -14,3 +14,11 @@ print(type(name))
 print(type(age))
 print(type(isMarried))
 print(type(gradePoints))
+
+#multiplication table from user input
+num = int(input("enter the number for which you want multiplication table\n"))
+for n in range(1,11):
+    print(f" {num} X {n} = {num*n}")
+
+
+
